66684107a987d03a3c3f87aac3ce1507f50a51a9
Reid <rlj0713@gmail.com>
2021-01-31 23:22:03 -0500

    commit tree b2d19949fe843ae1141f506a2660c1d99f818df6

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,37 @@
 
+class Artist
+    attr_accessor :name, :genre
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        array = []
+        Song.all.each do |song| 
+            if song.artist == self
+                array << song
+            end
+        end 
+        array
+    end
+
+    def new_song(name, genre)
+        new_song = Song.new(name, self, genre)
+    end
+
+    def genres
+        songs.map do |song|
+            song.genre
+        end
+    end
+
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
 
+class Genre
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @artist = artist
+        @@all << self
+    end
+
+    def songs
+        Song.all.select { |song| song.genre == self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        songs.map { |song| song.artist }
+    end
+
+
+end
